ab4b9e6b437cf9ea264e4100669cc1510f0e1584
herzliyaaa <herzliabarangan@gmail.com>
2023-04-13 11:21:59 +0800

    refactor: Organized the structure for the interfaces

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,29 +1,28 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type User = {
-    id: number;
-    name?: string;
-  };
-  
+import type { NextApiRequest, NextApiResponse } from "next";
+import { users } from "./data";
+import { User, ResponseError } from "../../interfaces";
 export default function userHandler(
   req: NextApiRequest,
-  res: NextApiResponse<User>
+  res: NextApiResponse<User | ResponseError>
 ) {
-  const { query, method } = req
-  const id = parseInt(query.id as string, 10)
-  const name = query.name as string
+  const { query, method } = req;
+  const { id } = query;
+  let person: User = users.find((p) => p.id === id);
+  console.log("🚀 ~ file: [id].ts:11 ~ person:", person);
 
   switch (method) {
-    case 'GET':
+    case "GET":
       // Get data from your database
-      res.status(200).json({ id, name: `User ${id}` })
-      break
-    case 'PUT':
+      person
+        ? res.status(200).json(person)
+        : res.status(404).json({ message: `User with id: ${id} not found.` });
+      break;
+    case "PUT":
       // Update or create data in your database
-      res.status(200).json({ id, name: name || `User ${id}` })
-      break
+      res.status(200).json(person);
+      break;
     default:
-      res.setHeader('Allow', ['GET', 'PUT'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+      res.setHeader("Allow", ["GET", "PUT"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,19 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type User = {
-  id: number;
-  name?: string;
-};
-
-const user: User[] = [
-  { id: 1, name: "Minnie" },
-  { id: 2, name: "Sky" },
-];
+import { users } from "./data"
+import { User } from "../../interfaces"
 
 export default function handler(
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse<User[]>
 ) {
-  res.status(200).json(user);
+  res.status(200).json(users);
 }
