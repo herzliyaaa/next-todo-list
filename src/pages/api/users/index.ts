21627f1ad7bfcaec9cc842aca75662997e245d52
herzliyaaa <herzliabarangan@gmail.com>
2023-04-13 14:25:14 +0800

    fix: minor fixes

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { users } from "./data"
-import  User  from "../../interfaces"
+import User from "@/interfaces";
 
 export default function handler(
   _req: NextApiRequest,
