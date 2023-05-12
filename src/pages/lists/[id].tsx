cd42ad1c2cd54d970f2d9db225d6614159234eaa
herzliyaaa <herzliabarangan@gmail.com>
2023-05-12 13:55:54 +0800

    Fixed ternary condition in todo list

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -11,11 +11,14 @@ function ListItem() {
 
   const markTodo = (id: number) => {
     console.log(`MARKED ${id}!`);
-    setTasks(
-      tasks.map((task) =>
-        task.taskId === id ? { ...task, completed: true } : task
-      )
-    );
+    setTasks((tasks: any) => {
+      const task = tasks.find((task: any) => {
+        return task.taskId === id;
+      });
+      task.completed = true;
+
+      return [...tasks];
+    });
   };
   useEffect(() => {
     const fetchTodoDetails = async () => {
@@ -34,7 +37,11 @@ function ListItem() {
     <main className="h-screen w-screen flex justify-center">
       <div className="flex flex-col p-5 w-1/2 max-[800px]:w-screen">
         <div className="flex justify-start items-start flex-col">
-          <BsArrowLeftShort size={40} onClick={() => router.back()} className="cursor-pointer" />
+          <BsArrowLeftShort
+            size={40}
+            onClick={() => router.back()}
+            className="cursor-pointer"
+          />
           {/* <button className="bg-indigo-500 h-[3rem] w-[3rem] flex justify-center items-center rounded-3xl hover:bg-indigo-400"></button> */}
         </div>
         <div className="flex pt-5 pb-5 justify-start items-center">
@@ -50,16 +57,22 @@ function ListItem() {
             return (
               <div
                 className={`p-4  mb-1 rounded-2xl border border-gray-700 bg-gray-50 h-auto dark:bg-[#4c5762] w-full ${
-                  task.completed == true ? "text-red" : ""
+                  task.completed ? "dark:bg-green-600" : ""
                 } `}
                 key={task.taskId}
               >
                 <div className="flex flex-row gap-2">
                   <span
                     onClick={() => markTodo(task.taskId)}
-                    className="cursor-pointer"
+                    className={`cursor-pointer `}
                   >
-                    <p className="text-sm text-white">{task.taskName}</p>
+                    <p
+                      className={`text-sm text-white ${
+                        task.completed ? "line-through" : ""
+                      }`}
+                    >
+                      {task.taskName}
+                    </p>
                   </span>
                 </div>
               </div>
